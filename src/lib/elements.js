3551b44bc360103138f8d2b54d4df95bc915428d
Muralidhar22 <muralidhar7794@gmail.com>
2022-04-30 22:24:36 +0530

    Map fixed and on load displaying ip

---
+++ src/lib/elements.js
@@ -0,0 +1,6 @@
+export const address = document.querySelector(".address .ip--value")
+export const location = document.querySelector(".location .ip--value")
+export const timezone = document.querySelector(".timezone .ip--value")
+export const isp = document.querySelector(".isp .ip--value")
+export const searchBtn = document.querySelector(".search-bar .search")
+
--- src/scripts.js
+++ src/scripts.js
@@ -1,41 +1,55 @@
+import displayInfo from "./lib/display.js"
+import { searchBtn } from "./lib/elements.js";
+// custom marker for the map
+const customMarker = L.icon({
+    iconUrl: '/src/images/icon-location.svg',
+    iconSize: [35, 45],
+})
+// onload of the window fetch the Map according to the viewer IP
 window.addEventListener('load', getMap);
 
+// search event listener
+searchBtn.addEventListener('click',getMap)
 
-function getMap(){
-    // MapId: cb763ab7aa613d55
-    // getLoc()
-    // const getLocDetails = getLoc()
-    // console.log(`Map is being loaded`)
-    // console.log(getLocDetails)
-    let latitude = 37.40599;
-    let longitude = -122.078514;
-    let mapOptions = {
-        center: [latitude, longitude],
+// display Map
+async function getMap(){
+    console.log(`Map is being loaded`)
+
+    const ipData = await getLoc()
+    displayInfo(ipData)
+    const latd = ipData.location.lat;
+    const lngd = ipData.location.lng;
+    const mapOptions = {
+        center: [latd, lngd],
         zoom: 13,
     }
-    // Map setup
-    var map = L.map('map').setView(mapOptions)
+// Map setup
+    var map = L.map('map').setView(mapOptions.center, mapOptions.zoom)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors and coded by <a href=""></a>'
+    }).addTo(map);
 
-L.marker([latitude, longitude]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+    L.marker(mapOptions.center,{icon: customMarker}).addTo(map);
 }
+
+// fetch Geo-location
 async function getLoc(){
-    const inputIp = document.querySelector('input[type="search"]').value
-    const inputValue = inputIp || await userIp()
+    // const inputIp = document.querySelector('input[type="search"]').value
+    // const inputValue = inputIp || await userIp()
    
-    // const endPoint = `https://geo.ipify.org/api/v2/country,city`
+    // const endpoint = `https://geo.ipify.org/api/v2/country,city`
     // const apiKey = `at_6cqSy2CpvjQ0xkfTvbnkFz4Zywo0k`
     // const query = `?apiKey=${apiKey}&ipAddress=${inputValue}`
-    // return `location details...`
+    
+    // const res = await fetch(`${endpoint}${query}`)
+    // return await res.json()
+   
 }
+// fetch current viewer IP
 async function userIp(){
     try{
-        let res = await fetch('https://ipapi.co/ip/')
+        const res = await fetch('https://ipapi.co/ip/')
         return await res.text()
     }
     catch(e){
