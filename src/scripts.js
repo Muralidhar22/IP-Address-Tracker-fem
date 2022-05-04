8f3ad1b258bc9d82647d0e187f67cc3cf26df602
Muralidhar22 <muralidhar7794@gmail.com>
2022-05-04 13:00:35 +0530

    marker fixed

--- src/scripts.js
+++ src/scripts.js
@@ -2,12 +2,11 @@ import displayInfo from "./lib/display.js"
 import { searchBtn } from "./lib/elements.js";
 // custom marker for the map
 const customMarker = L.icon({
-    iconUrl: '/src/images/icon-location.svg',
+    iconUrl: './images/icon-location.svg',
     iconSize: [35, 45],
 })
 // onload of the window fetch the Map according to the viewer IP
 window.addEventListener('load', getMap);
-
 // search event listener
 searchBtn.addEventListener('click',getMap)
 
@@ -15,33 +14,39 @@ searchBtn.addEventListener('click',getMap)
 async function getMap(){
    searchBtn.classList.add("disabled")
    searchBtn.disabled=true
-
+// get location
     const ipData = await getLoc()
-//   
-console.log(ipData)
-// 
+
     searchBtn.classList.remove("disabled")
     searchBtn.disabled=false
-    displayInfo(ipData)
-    const latd = ipData.location.lat;
-    const lngd = ipData.location.lng;
-    const mapOptions = {
-        center: [latd, lngd],
-        zoom: 13,
-    }
-// Map setup
-    let container = L.DomUtil.get('map');
+ if(ipData.code === 422){
+    document.querySelector('input[type="search"]').value = ''
+    document.querySelector('input[type="search"]').placeholder= ipData.messages
+ }
+else{
+        // display the infomration on the page
+        displayInfo(ipData)
+        const latd = ipData.location.lat;
+        const lngd = ipData.location.lng;
+        const mapOptions = {
+            center: [latd, lngd],
+            zoom: 13,
+        }
+        // Map setup
+        let container = L.DomUtil.get('map');
 
-    if(container != null){
-        container._leaflet_id = null;
-    }
-    let map = L.map('map').setView(mapOptions.center, mapOptions.zoom)
+        if(container != null){
+            container._leaflet_id = null;
+        }
+        let map = L.map('map').setView(mapOptions.center, mapOptions.zoom)
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors and coded by <a href=""></a>'
-    }).addTo(map);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors and coded by <a href=""></a>'
+        }).addTo(map);
 
-    L.marker(mapOptions.center,{icon: customMarker}).addTo(map);
+        L.marker(mapOptions.center,{icon: customMarker}).addTo(map);
+}
+   
 
 }
 
@@ -59,8 +64,9 @@ async function getLoc(){
     }
   catch(e){
       console.log(e)
-      document.querySelector('input[type="search"]').placeholder= "Sorry, not found"
       document.querySelector('input[type="search"]').value = ''
+      document.querySelector('input[type="search"]').placeholder= "Sorry, not found"
+      
       return null;
   }
    
